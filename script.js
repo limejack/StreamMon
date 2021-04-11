8bf8b8b14511e90e47f4d56b0059efe4cfdc0e78
limejack <82292253+limejack@users.noreply.github.com>
2021-04-11 11:51:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function clicked(){
     var x = document.getElementsByClassName("total");
     for(var i = 0; i < x.length; i ++)
     {
-        alert(x[i].firstChild.value());
+        alert(x[i].value);
     }
 }
 document.getElementById("main_form").onsubmit = clicked();

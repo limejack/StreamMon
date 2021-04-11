9cf8141f5c721ac02635b47f9fc86d3bd961cd5a
IncredHam <77354590+IncredHam@users.noreply.github.com>
2021-04-11 16:11:40 -0400

    Update dichotomusform.js

--- dichotomusform.js
+++ dichotomusform.js
@@ -13,10 +13,18 @@
 //         };
 function chooseNextQuestion(q_name, option) {
     if (q_name == "q1") {
-        if (option == "no-legs") {
-            for(var i = 1; i <= 40; i++) {
-                document.getElementById("f" + i).disabled = true;
-            }
+        for(var i = 1; i <= 40; i++) {
+            document.getElementById("f" + i).disabled = true;
         }
+        if (option == "no-legs") {
+            document.getElementById("f3").disabled = false;
+            document.getElementById("f4").disabled = false;
+        }
+        if (option == "legs") {
+            document.getElementById("f5").disabled = false;
+            document.getElementById("f6").disabled = false;
+        }
+        document.getElementById("f1").disabled = false;
+        document.getElementById("f2").disabled = false;
     }
 };
